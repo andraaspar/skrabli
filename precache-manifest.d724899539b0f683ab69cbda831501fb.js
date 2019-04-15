d66286ec69a67ef4842607e72124f20802fa81d3
András Parditka <andraaspar@gmail.com>
2019-04-15 20:20:50 +0200

    Updates

---
+++ precache-manifest.d724899539b0f683ab69cbda831501fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d13cd337d331aeda72d8",
+    "url": "/skrabli/static/js/runtime~main.6f60a26b.js"
+  },
+  {
+    "revision": "bd76070c6f96f17f4cf1",
+    "url": "/skrabli/static/js/main.7f087568.chunk.js"
+  },
+  {
+    "revision": "b4f89c067493d8656b86",
+    "url": "/skrabli/static/js/2.112ce33d.chunk.js"
+  },
+  {
+    "revision": "bd76070c6f96f17f4cf1",
+    "url": "/skrabli/static/css/main.45148e64.chunk.css"
+  },
+  {
+    "revision": "b4f89c067493d8656b86",
+    "url": "/skrabli/static/css/2.a3ab5a97.chunk.css"
+  },
+  {
+    "revision": "7ac34dab1699ef1d2010043694a6c88d",
+    "url": "/skrabli/index.html"
+  }
+];
