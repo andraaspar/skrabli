6a6526f2faa4d4a77527ab7e0ca6b4b2fb6e28aa
András Parditka <andraaspar@gmail.com>
2023-07-04 15:48:14 +0200

    Fix build.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: './',
 	plugins: [
 		vue({
 			template: {
@@ -15,6 +16,7 @@ export default defineConfig({
 		}),
 		VitePWA({
 			registerType: 'autoUpdate',
+			filename: 'service-worker.js',
 			includeAssets: [
 				'favicon.ico' /* , 'apple-touch-icon.png', 'masked-icon.svg' */,
 			],
