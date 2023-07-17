404106aec85721370fd58dc132769a9d0b5a181b
András Parditka <andraaspar@gmail.com>
2023-07-17 17:16:56 +0200

    Formatted.

--- src/fun/theOtherDirection.ts
+++ src/fun/theOtherDirection.ts
@@ -1,7 +1,5 @@
 import { Direction } from '../model/Direction'
 
 export function theOtherDirection(d: Direction) {
-	return d === Direction.Horizontal
-		? Direction.Vertical
-		: Direction.Horizontal
+	return d === Direction.Horizontal ? Direction.Vertical : Direction.Horizontal
 }
